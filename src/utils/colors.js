ad39a106ad0d695738c7bf1413dc59bd1afd309b
ilha12 <ilhaadrees12@gmail.com>
2024-09-27 23:44:52 +0500

    login

---
+++ src/utils/colors.js
@@ -0,0 +1,14 @@
+export const colors={
+    white:"#FFFFFF",
+    primary:"#45484A",
+    secondary:"#AEB5BB",
+    gray:"#D9D9D9",
+    black:"#000000",
+    red:"#FF0000",
+    Orange:"#ff810d",
+    Creamsicle:"#F28E2B",
+    Tangerine:"#F2571B",
+    Coffee:"#6B4226",
+    Ten:"#D2B48C",
+    black:"#000000",
+}
