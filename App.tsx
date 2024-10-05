3ae16eaf0e76411b18734943e2ba0d18f1d003cb
ilha12 <ilhaadrees12@gmail.com>
2024-10-05 10:42:32 +0500

    Created new splash screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
+import SplashScreen from 'react-native-splash-screen';
 import './gesture-handler';
 import MainNavigation from './src/navigation/MainNavigation';
+import {useEffect} from 'react';
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+  }, []);
+
   return <MainNavigation />;
 };
 
